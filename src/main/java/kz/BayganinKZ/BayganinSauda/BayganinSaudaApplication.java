b60d481139c453a82a93b2a59fcc7a0cc597b27f
Daniyar <daniyar.doskulov@narxoz.kz>
2023-02-19 04:24:25 +0600

    first

---
+++ src/main/java/kz/BayganinKZ/BayganinSauda/BayganinSaudaApplication.java
@@ -0,0 +1,13 @@
+package kz.BayganinKZ.BayganinSauda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BayganinSaudaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BayganinSaudaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kz/BayganinKZ/BayganinSauda/BayganinSaudaApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.BayganinKZ.BayganinSauda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BayganinSaudaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
